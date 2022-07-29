f29d9d5c86527396bf085567fa7e529ef5a0f996
andrepreira <andre.pereira.fisica@gmail.com>
2022-07-29 07:53:42 -0300

    esaj

--- tribunais_esaj/esaj.py
+++ tribunais_esaj/esaj.py
@@ -1,10 +1,15 @@
 
 
 from drivers.firefox.driver_firefox import WebDriver
+from selenium.webdriver.common.by import By
 
 webdriver = WebDriver()
 
-url = 'https://esaj.tjsc.jus.br/cpopg/open.do'
-webdriver.driver.get(url)
+url1 = 'https://esaj.tjsc.jus.br/cpopg/open.do'
+url2 = 'https://esaj.tjms.jus.br/cpopg5/open.do'
+url3 = 'https://consultasaj.tjam.jus.br/cpopg/open.do'
+webdriver.driver.get(url2)
 
-webdriver.esperar_ate_elemento_ficar_visivel('#rc-anchor-container')
+# webdriver.esperar_ate_elemento_ficar_visivel('.form__consultar-processos__item--captcha')
+webdriver.esperar_ate_elemento_ficar_visivel('.grecaptcha-badge')
+webdriver.driver.find_element(By.CSS_SELECTOR,value='.grecaptcha-badge')
